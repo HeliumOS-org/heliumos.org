2ff727123851fcb00a838db8a245e56330b58202
Isaac Beverly <imbev@protonmail.com>
2024-01-17 20:10:30 +0000

    Add theme

--- heliumos_website/tailwind.config.js
+++ heliumos_website/tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../**/*.{html}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
+    content: ["**/*.html"],
+    theme: {
+        extend: {},
+    },
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    "primary": "#3daee9",
+                    "secondary": "#eff0f1",
+                    "accent": "#3daee9",
+                    "neutral": "#eff0f1",
+                    "base-100": "#fcfcfc",
+                    "info": "#3daee9",
+                    "success": "#27ae60",
+                    "warning": "#f67400",
+                    "error": "#da4453",
+                },
+            },
+        ],
+    },
+    plugins: [require("daisyui")],
 }
-

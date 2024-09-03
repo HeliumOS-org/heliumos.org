33b367c42b380c26e27d401d7ff211a58ff698f2
Isaac Beverly <imbev@protonmail.com>
2024-09-03 13:18:01 -0500

    Add hardware to sitemap

--- heliumos_website/www/sitemaps.py
+++ heliumos_website/www/sitemaps.py
@@ -2,12 +2,12 @@ from django.contrib.sitemaps import Sitemap
 from django.urls.base import reverse
 
 from .models import BlogPost
-from .views import download, docs, index, roadmap
+from .views import download, docs, hardware, index, roadmap
 
 
 class MainSitemap(Sitemap):
     def items(self):
-        return [download, docs, index, roadmap]
+        return [download, docs, index, roadmap, hardware]
 
     def location(self, item):
         return reverse(item)
